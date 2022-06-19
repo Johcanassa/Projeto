ffda62f01408e37ca04405078436a1778a4e1bd9
Joyce Canassa <joh.fabiano@gmail.com>
2022-06-18 21:15:09 -0300

    Conexão do Bem

--- app/src/main/java/com/example/conexaodobem/Gaadi.java
+++ app/src/main/java/com/example/conexaodobem/Gaadi.java
@@ -3,6 +3,7 @@ package com.example.conexaodobem;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
@@ -10,6 +11,7 @@ import android.widget.Toast;
 
 public class Gaadi extends AppCompatActivity {
     ImageView imgClick;
+    ImageView imgwhats;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,6 +22,17 @@ public class Gaadi extends AppCompatActivity {
             public void onClick(View v) {
                 Intent it = new Intent(Gaadi.this, MapsActivity.class);
                 startActivity(it);
+            }
+        });
+        setContentView(R.layout.activity_gaadi);
+        imgwhats=(ImageView) findViewById(R.id.idwhats);
+        imgwhats.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url ="https://api.whatsapp.com/send?phone=5515998439134";
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse(url));
+                startActivity((intent));
             }
         });
     }
--- app/src/main/java/com/example/conexaodobem/doutoresdaalegria.java
+++ app/src/main/java/com/example/conexaodobem/doutoresdaalegria.java
@@ -2,13 +2,28 @@ package com.example.conexaodobem;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class doutoresdaalegria extends AppCompatActivity {
+    ImageView imgwhats;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_doutoresdaalegria);
+        imgwhats=(ImageView) findViewById(R.id.idwhats);
+        imgwhats.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url ="https://api.whatsapp.com/send?phone=5515998075732";
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse(url));
+                startActivity((intent));
+            }
+        });
     }
 }
